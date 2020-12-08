c7dc99fcad185647ab0350fbe7e1e2f2ef194068
August Thorell <augustthorell@gmail.com>
2020-12-08 02:28:36 +0100

    added html and css

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+function toggleNavBar() {
+    mainNav.classList.toggle('active');
+    navBarToggle.classList.toggle('opened');
+}
+
+navBarToggle.addEventListener('click', toggleNavBar);
+
